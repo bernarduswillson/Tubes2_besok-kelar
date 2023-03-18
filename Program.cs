79bcbdf2e1b9ae47bfd611e2979b90db0e6bdd0e
henryanandsr <13521004@std.stei.itb.ac.id>
2023-03-19 01:38:49 +0700

    feat : Add BFS Algorithm

--- Program.cs
+++ Program.cs
@@ -1,85 +1,326 @@
 ﻿using System;
 using System.IO;
-class Node
+class BFS
 {
-    private Node childTop;
-    private Node childBottom;
-    private Node childLeft;
-    private Node childRight;
-    private Node parent;
-    static int n;
-    private int id;
-    private int number;
-    public Node(int num)
+    /*
+        1 -> kanan
+        2 -> bawah
+        3 -> kiri
+        4 -> atas
+    */
+    private Queue<String> path;
+    private Queue<Path> coorMap;
+    private int x;
+    private int[,] maze;
+    private int y;
+    private int count;
+    public BFS(int x, int y, int[,] arr, int t)
     {
-        this.childTop = null;
-        this.childBottom = null;
-        this.childLeft = null;
-        this.childRight = null;
-        this.parent = null;
-        this.number = num;
-        this.id = n;
-        n++;
+        this.x = x;
+        this.y = y;
+        this.maze = arr;
+        this.count = t;
+        this.coorMap = new Queue<Path>();
+        //Lakukan pengecekan pada kiri start
+        if (this.x-1 >= 0 && this.maze[x-1,y] != 0)
+        {
+            Path temp = new Path(this.maze);
+            if(this.maze[x-1,y] == 3)
+            {
+                temp.pickTreasure(x-1,y);
+            }
+            temp.Append(x,y);
+            temp.Append(x-1,y);
+            // path.Enqueue("L");
+            this.coorMap.Enqueue(temp);
+            // Console.Write("L");
+        }
+        //Lakukan pengecekan pada kanan start
+        if (this.x+1 < this.maze.GetLength(1) && this.maze[x+1,y] != 0)
+        {
+            // Console.WriteLine(this.maze.GetLength(0));
+            // Console.WriteLine(x+1)/;
+            Path temp = new Path(this.maze);
+            if(this.maze[x+1,y] == 3)
+            {
+                temp.pickTreasure(x+1,y);
+            }
+            temp.Append(x,y);
+            temp.Append(x+1,y);
+            // path.Enqueue("R");
+            coorMap.Enqueue(temp);
+            // Console.Write("R");
+        }
+        //Lakukan pengecekan pada bawah start        
+        if (this.y+1 < this.maze.GetLength(0) && this.maze[x,y+1] != 0)
+        {
+            Path temp = new Path(this.maze);
+            if(this.maze[x,y+1] == 3)
+            {
+                temp.pickTreasure(x,y+1);
+            }
+            temp.Append(x,y);
+            temp.Append(x,y+1);
+            // path.Enqueue("D");
+            coorMap.Enqueue(temp);
+            // Console.Write("D");
+        }
+        //Lakukan pengecekan pada atas start        
+        if (this.y-1 >= 0 && this.maze[x,y-1] != 0)
+        {
+            Path temp = new Path(this.maze);
+            if(this.maze[x,y-1] == 3)
+            {
+                temp.pickTreasure(x,y-1);
+            }
+            temp.Append(x,y);
+            temp.Append(x,y-1);
+            // path.Enqueue("U");
+            coorMap.Enqueue(temp);
+            // Console.Write("Up");
+        }
+        // Path temp2 = coorMap.Dequeue();
+        // temp2.displayCoord();
+        // Console.WriteLine(temp2.getCount());
+        // temp2.displayMaze();
+        // Path temp3 = coorMap.Dequeue();
+        // temp3.displayCoord();
+        
     }
-    // public void searchChild()
-    // {
-
-    // }
-    public Node(int num, Node parent)
+    public Queue<String> getPath()
     {
-        this.number = num;
-        this.childTop = null;
-        this.childBottom = null;
-        this.childLeft = null;
-        this.childRight = null;
-        this.parent = parent;
+        return path;
     }
-    // public void addChild(Node other)
-    // {
-    //     this.child = other;
-    // }
-    public int getID()
+    public bool isDone(int[,] arr)
     {
-        return this.id;
+        for (int i = 0 ; i < arr.GetLength(1); i ++)
+        {
+            for (int j =  0 ; j < arr.GetLength(0) ; j++)
+            {
+                if (arr[i,j] == 3)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
-    public int getNum()
+    public void findPath()
     {
-        return this.number;
+        bool found = false;
+        int[] tempCoor;
+        while (!found)
+        {
+            //Ambil tiap jalurnya
+            // Console.WriteLine(coorMap.Count);
+            Path tempPath = coorMap.Dequeue();
+            Path tempP1 = new Path(tempPath);
+            Path tempP2 = new Path(tempPath);
+            Path tempP3 = new Path(tempPath);
+            Path tempP4 = new Path(tempPath);
+            tempCoor = tempPath.getEl(tempPath.getIdx());
+            // Console.WriteLine("==========================");
+            // tempPath.displayCoord();
+            // tempPath.displayMaze();
+            // Console.WriteLine(tempPath.getCount());
+            // Console.WriteLine("==========================");
+            //Cek apakah sudah semua terambil
+            if (isDone(tempPath.getMaze()))
+            {
+                // Console.Write("PP");
+                tempPath.displayCoord();
+                tempPath.displayMaze();
+                found = true;
+                break; 
+            }
+            //cek bagian atas
+            if (tempPath.canTravel(tempCoor[0],tempCoor[1]-1))
+            {
+                //cek apakah treasure
+                Path append = new Path(this.maze);
+                append = tempP1;
+                if (append.isTreasure(tempCoor[0],tempCoor[1]-1))
+                {
+                    append.pickTreasure(tempCoor[0],tempCoor[1]-1);
+                }
+                append.Append(tempCoor[0],tempCoor[1]-1);
+                // Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                // append.displayCoord();
+                // Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                coorMap.Enqueue(append);
+            }
+            //cek bagian bawah
+            if (tempPath.canTravel(tempCoor[0],tempCoor[1]+1))
+            {
+                //cek apakah treasure
+                Path append = new Path(this.maze);
+                append = tempP2;
+                if (append.isTreasure(tempCoor[0],tempCoor[1]+1))
+                {
+                    append.pickTreasure(tempCoor[0],tempCoor[1]+1);
+                }
+                append.Append(tempCoor[0],tempCoor[1]+1);
+                coorMap.Enqueue(append);
+                // Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                // append.displayCoord();
+                // Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            }
+            //cek bagian kiri
+            if (tempPath.canTravel(tempCoor[0]-1,tempCoor[1]))
+            {
+                //cek apakah treasure
+                Path append = new Path(this.maze);
+                append = tempP3;
+                if (append.isTreasure(tempCoor[0]-1,tempCoor[1]))
+                {
+                    append.pickTreasure(tempCoor[0]-1,tempCoor[1]);
+                }
+                append.Append(tempCoor[0]-1,tempCoor[1]);
+                coorMap.Enqueue(append);
+            }
+            //cek bagian kanan
+            if (tempPath.canTravel(tempCoor[0]+1,tempCoor[1]))
+            {
+                //cek apakah treasure
+                Path append = new Path(this.maze);
+                append = tempP4;
+                if (append.isTreasure(tempCoor[0]+1,tempCoor[1]))
+                {
+                    append.pickTreasure(tempCoor[0]+1,tempCoor[1]);
+                }
+                append.Append(tempCoor[0]+1,tempCoor[1]);
+                coorMap.Enqueue(append);
+            }
+        }
     }
 }
-class Graph
+class Path
 {
-    Node startNode;
-    public Graph(Node startNode)
+    private int[,] arr;
+    private int[,] maze;
+    private bool filled;
+    private int count;
+    private int idx;
+    public int[,] getArr()
     {
-        this.startNode = startNode;
+        return arr;
     }
-}
-class Matrix
-{
-    private int[,] matrix;
-    public Matrix(int row, int col)
+    public Path(int[,] maze)
     {
-        this.matrix = new int[row,col];
+        this.arr = new int[1,2];
+        this.maze = maze;
+        this.filled = false;
+        this.count = 0;
+        this.idx = -1;
     }
-    public int getElement(int row, int col)
+    public Path(Path other)
     {
-        return matrix[row,col];
+        this.arr = (int[,])other.arr.Clone();
+        this.maze = (int[,])other.maze.Clone();
+        this.filled = other.filled;
+        this.count = other.count;
+        this.idx = other.idx;
     }
-    public void setElement(int row, int col, int el)
+    public void displayMaze()
     {
-        matrix[row,col] = el;
+        for (int i = 0 ; i<maze.GetLength(1) ; i++)
+        {
+            for (int j = 0 ;  j < maze.GetLength(0) ; j++)
+            {
+                Console.Write(maze[i,j] + " ");
+            }
+            Console.WriteLine();
+        }
     }
-    public bool isAdjacent(int r1, int c1, int r2, int c2)
+    public void displayCoord()
     {
-        if (r1-1 == r2 || r1 + 1 == r2 || c1 -1 == c2 || c1 + 1 == c2)
+        for (int i = 0 ; i < arr.GetLength(0) ;i++)
+        {
+            for (int j = 0 ; j < 2 ;j++)
+            {
+                Console.Write(arr[i,j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+    public int getIdx()
+    {
+        return this.idx;
+    }
+    public bool isTreasure(int x, int y)
+    {
+        if (maze[x,y] == 3)
         {
             return true;
         }
-        else 
+        else
         {
             return false;
         }
+    }
+    public bool canTravel(int x, int y)
+    {
+        if (x>=0 && x<maze.GetLength(1) && y>=0 && y<maze.GetLength(0))
+        {
+            if (maze[x,y] != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public void Append(int a, int b)
+    {
+        if (this.filled)
+        {
+            // Console.WriteLine(a);
+            // Console.WriteLine(b);
+            int[,] temp = new int[this.arr.GetLength(0)+1, this.arr.GetLength(1)];
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    temp[i, j] = arr[i, j];
+                }
+            }
+            temp[arr.GetLength(0),0] = a;
+            temp[arr.GetLength(0),1] = b;
+            this.arr = temp;
+        }
+        else
+        {
+            this.arr[0,0] = a;
+            this.arr[0,1] = b;
+            this.filled = true;
+        }
+        this.idx++;
+    }
+    public int[] getEl(int idx)
+    {
+        int[] ret = new int[2];
+        ret[0] = this.arr[idx,0];
+        ret[1] = this.arr[idx,1];
+        return ret;
+    }
+    public int[,] getMaze()
+    {
+        return maze;
+    }
+    public void pickTreasure(int x, int y)
+    {
+        this.maze[x,y] = 2;
+        count++;
+    }
+    public int getCount()
+    {
+        return count;
     }
 }
 /*
@@ -115,6 +356,7 @@ class Program
         int charCode;
         int idRow = 0;
         int idCol = 0;
+        int count = 0;
         StreamReader reader2 = new StreamReader(filePath);
         while ((charCode = reader2.Read()) != -1)
         {
@@ -138,6 +380,7 @@ class Program
                 else if (c == 'T')
                 {
                     arr[idRow,idCol] = 3;
+                    count++;
                 }
                 else
                 {
@@ -150,27 +393,34 @@ class Program
                     idCol= 0;
                 }
             }
-            // Console.Write(c);
         }
-        // Console.WriteLine();
         reader.Close();
         reader2.Close();
+        // Node root;
         //bikin graph
-        Graph mainGraph;
-        Node pointerNode;
+        int x = 0;
+        int y = 0;
+        int treasure = 0;
         for (int i = 0 ; i < row ; i++)
         {
             for (int j = 0 ; j < col ; j++)
             {
                 if(arr[i,j]==1)
                 {
-                    Node temp = new Node(3);
-                    mainGraph = new Graph(temp);
-                    // Console.Write(c);
+                   x = i;
+                   y = j;
+                }
+                if(arr[i,j] == 3)
+                {
+                    treasure++;
                 }
                 Console.Write(arr[i,j]+" ");
             }
             Console.WriteLine();
         }
+        // Console.WriteLine(treasure);
+        BFS root = new BFS(x,y,arr,treasure);
+        Console.WriteLine("Koordinat untuk mengambil semua treasure : ");
+        root.findPath();
     }
 }
---
+++ tempCodeRunnerFile.cs
@@ -0,0 +1,9 @@
+ // public Node(int num, Node parent)
+    // {
+    //     this.number = num;
+    //     this.childTop = null;
+    //     this.childBottom = null;
+    //     this.childLeft = null;
+    //     this.childRight = null;
+    //     this.parent = parent;
+    // }
